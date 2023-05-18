53ab4c8191a35d5ddca3329db5c9eb778f2652e9
KostiaKyryliv <kyrylivkostia23@gmail.com>
2023-05-18 12:00:08 +0300

    Litle game

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trypull
+{
+    internal class Program
+    {
+        static void obrobka(string vvedenny, string keyword)
+        {
+            bool f = false;
+            for (int i=0; i<3;i++)
+            {
+                if(vvedenny == keyword)
+                {
+                    System.Console.WriteLine("You win!");
+                    f = false;
+                }
+                else
+                {
+                    System.Console.Write("Enter line again: ");
+                    vvedenny = Console.ReadLine();
+                }
+                if (vvedenny != keyword && i==2)
+                {
+                    System.Console.WriteLine("you loose");
+                }
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            string ryd = "yes";
+            Console.Write("Enter your chois(you have 3 trys): ");
+            string chois = Console.ReadLine();
+            obrobka(chois, ryd);
+        }
+    }
+}
